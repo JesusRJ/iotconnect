13f2498f415519741c276e867c4f57ead4830631
Reginaldo Jesus <reginaldo.jesus@gmail.com>
2017-10-10 23:55:13 -0300

    Implementação de mensagens via broadcast

---
+++ network/broadcast.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "net"
+  "os"
+)
+
+import (
+  "golang.org/x/net/ipv4"
+)
+
+func Announce() {
+  laddr, err := net.ResolveUDPAddr("udp", ":0")
+  if err != nil {
+    log.Fatal(err)
+  }
+  maddr, err := net.ResolveUDPAddr("udp4", "255.255.255.255:8089")
+  if err != nil {
+    log.Fatal(err)
+  }
+  c, err := net.ListenUDP("udp4", laddr)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer c.Close()
+  scanner := bufio.NewScanner(os.Stdin)
+  for scanner.Scan() {
+    {
+      text := scanner.Text()
+      _, err := c.WriteToUDP([]byte(text), maddr)
+      if err != nil {
+        log.Fatal(err)
+      }
+    }
+  }
+}
+
+func Listen() {
+  c, err := net.ListenPacket("udp4", "0.0.0.0:8089") // mDNS over UDP
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer c.Close()
+  p := ipv4.NewPacketConn(c)
+
+  ifaces, err := net.Interfaces()
+  if err != nil {
+    log.Fatal(err)
+  }
+  //TODO lets user choose iface or select one with internet access.
+  //TODO OR! listen on all ports (need agreed range).
+
+  //TODO add layer of abstraction for game mechanics input output
+  for _, iface := range ifaces {
+    fmt.Printf("%v\n", iface.Name)
+    go listenOnSpecificInterface(p, &iface)
+  }
+  // for {
+  // }
+}
+
+func listenOnSpecificInterface(p *ipv4.PacketConn, iface *net.Interface) {
+  if iface.Name != "enp2s0" {
+    return
+  }
+
+  mDNSLinkLocal := net.UDPAddr{IP: net.IPv4(224, 0, 0, 251)}
+  if err := p.JoinGroup(iface, &mDNSLinkLocal); err != nil {
+    log.Fatal(err)
+  }
+  defer p.LeaveGroup(iface, &mDNSLinkLocal)
+  if err := p.SetControlMessage(ipv4.FlagDst, true); err != nil {
+    log.Fatal(err)
+  }
+
+  b := make([]byte, 1500)
+  for {
+    fmt.Printf("Waiting for data on interface: %v\n", iface.Name)
+    n, cm, peer, err := p.ReadFrom(b)
+    fmt.Printf("Received on interface %v: %v \n", iface.Name, string(b[:n]))
+    if err != nil {
+      log.Fatal(err)
+    }
+    if !cm.Dst.IsMulticast() || !cm.Dst.Equal(mDNSLinkLocal.IP) {
+      continue
+    }
+    answers := []byte("FAKE-MDNS-ANSWERS") // fake mDNS answers, you need to implement this
+    if _, err := p.WriteTo(answers, nil, peer); err != nil {
+      log.Fatal(err)
+    }
+  }
+}
+
+func main() {
+  // Listen()
+  Announce()
+}
---
+++ network/udpconnect.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+  "fmt"
+  "golang.org/x/net/ipv4"
+  "net"
+)
+
+func main() {
+  ipv4Addr := &net.UDPAddr{IP: net.IPv4(255, 255, 255, 255), Port: 8089}
+  conn, err := net.ListenUDP("udp4", ipv4Addr)
+  if err != nil {
+    fmt.Printf("ListenUDP error %v\n", err)
+    return
+  }
+
+  pc := ipv4.NewPacketConn(conn)
+
+  // assume your have a interface named enp2s0
+  iface, err := net.InterfaceByName("enp2s0")
+  if err != nil {
+    fmt.Printf("can't find specified interface %v\n", err)
+    return
+  }
+
+  if err := pc.JoinGroup(iface, &net.UDPAddr{IP: net.IPv4(255, 255, 255, 255)}); err != nil {
+    fmt.Printf("%v\n", err)
+    return
+  }
+
+  // test
+  if loop, err := pc.MulticastLoopback(); err == nil {
+    fmt.Printf("MulticastLoopback status:%v\n", loop)
+    if !loop {
+      if err := pc.SetMulticastLoopback(true); err != nil {
+        fmt.Printf("SetMulticastLoopback error:%v\n", err)
+      }
+    }
+  }
+
+  if _, err := conn.WriteTo([]byte("hello"), ipv4Addr); err != nil {
+    fmt.Printf("Write failed, %v\n", err)
+  }
+
+  buf := make([]byte, 1024)
+  for {
+    if n, addr, err := conn.ReadFrom(buf); err != nil {
+      fmt.Printf("error %v", err)
+    } else {
+      fmt.Printf("recv %s from %v\n", string(buf[:n]), addr)
+    }
+  }
+
+  return
+}
